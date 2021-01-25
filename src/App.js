20904e515462da8b7757469accf275b62722bd1c
mihiran <154015E@uom.lk>
2021-01-25 22:11:58 +0530

    use react create element in app.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi,I am a react App</h1>
-      </div>
-    );
-  }
+     /*return (
+       <div className="App">
+         <h1>Hi,I am a react App</h1>
+       </div>
+     );*/
+     return ( React.createElement('div',null,React.createElement('h1',null,'Hello')));
+  } 
 }
 
 export default App;
