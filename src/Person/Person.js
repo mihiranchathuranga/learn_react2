2953fbf359a04e5910e7aebddb767979f6ce393b
mihiran <154015E@uom.lk>
2021-01-26 01:34:14 +0530

    wrap retrun inside div tags

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const person = (props) =>{
-    return <p>I'm  {props.name} and I'm {props.age} years old. </p>
+    return (
+        <div>
+            <p>I'm  {props.name} and I'm {props.age} years old. </p>
+        </div>
+    )
     
 }
 
