d8393980951c076f0486f5e0447eefb41f813490
mihiran <154015E@uom.lk>
2021-01-26 10:29:50 +0530

    create the setState method inside the App.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,28 @@ class App extends Component {
       { name: 'Shashika' , age: 28},
       { name: 'Nalin' , age: 27},
       { name: 'Saduni', age: 25}
-    ]
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () =>{
+    //console.log('Was clicked');
+    //this.state.persons[0].name = 'Mihiran'; do not use this
+    this.setState({
+      persons: [
+        { name: 'Upul' , age: 28},
+        { name: 'Pasan' , age: 27},
+        { name: 'Madhu', age: 25}
+      ]
+    })
+
   }
   render() {
      return (
        <div className="App">
          <h1>Hi,I am a react App</h1>
          <p>This is really working!</p>
+         <button onClick={this.switchNameHandler}>Switch Name</button>
          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I am a good Programmer</Person>
          <Person name={this.state.persons[2].name} age={this.state.persons[2].name}/>
